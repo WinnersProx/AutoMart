5af0e3f71c0ac0f8f552041eeda21ed6731a6a41
WINNERSPROX <bihames4vainqueur@gmail.com>
2019-05-16 05:08:51 -0700

    bug while merging

--- scripts/main.es6.js
+++ scripts/main.es6.js
@@ -2,34 +2,26 @@
 const togglerButton = document.querySelector('div.toggler');
 const toggleDropdown = document.querySelectorAll('.dropdown-toggle');
 const dropdownBody =document.querySelector('.dropdown-menu');
-let body = document.body;
-let html = document.documentElement;
-let initialize = () => {
-    console.log("Loaded");
-}
+
 // control window scrolls
-const  scrollState = () =>{
-	let windowInnerHeightX = window.innerHeight;
-	console.log(windowInnerHeightX);
-	if(body.scrollTop > 300 || html.scrollTop > 300)
-		togglerButton.classList.add('show');
+
+window.addEventListener('scroll', () =>{
+	if((window.pageYOffset * 2) > window.innerHeight)
+		togglerButton.classList.add('show');	
 	else
 		togglerButton.classList.remove('show');
-}
-window.scroll(scrollState());
+});
 
-
-
-const scrolltoTop = () => {
-	let scrollTop = Math.round(body.scrollTop || html.scrollTop);
-	window.scrollTop -= scrollTop;
-}
 
 togglerButton.addEventListener('click', () => {
-	// Scroll to the top
-	scrolltoTop();
+	// Smooth scroll to the top
+	window.scroll({
+	top: 0, 
+	left: 0, 
+	behavior: 'smooth'
+});
 })
-
+// Dropdowns implementation
 toggleDropdown.forEach((current,index) => {
 	current.addEventListener('click', e => {
 		let target = e.target.parentNode.children[1];
