8d371a7aa8566f1c6d7d3f7bc81ba34dbec3d2b3
WINNERSPROX <bihames4vainqueur@gmail.com>
2019-05-28 21:11:35 -0700

    complete signup

--- api/v1/routes/endpoints/auth.js
+++ api/v1/routes/endpoints/auth.js
@@ -3,7 +3,7 @@ import authController from '../../controllers/auth_controller'
 import Validations from '../../middlewares/authValidation'
 
 const routes = express.Router()
-// hence we'll be using customized middlewares to validate all the user requests
+// hence we'll be using customized middlewares to validate all the auth requests
 routes
 .post('/auth/signup', Validations.validateUser, authController.signup)
 .post('/auth/signin', Validations.validateSignin, authController.signin)
