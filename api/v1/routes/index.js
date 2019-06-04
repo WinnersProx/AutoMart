2c5becb00283b871362137586822931237b40eeb
WINNERSPROX <bihames4vainqueur@gmail.com>
2019-06-04 10:42:35 -0700

    user can flag or report a posted car ad [finishes #166318900]

--- api/v1/routes/index.js
+++ api/v1/routes/index.js
@@ -2,11 +2,13 @@ import express from 'express'
 import authRouter from './auth'
 import carsRouter from './cars'
 import ordersRouter from './orders'
+import flagsRouter from './flags'
 const router = express.Router()
 
 router.use('/api/v1/', router)
 router.use('/', authRouter)
 router.use('/', carsRouter)
 router.use('/', ordersRouter)
+router.use('/', flagsRouter)
 
 export default router
